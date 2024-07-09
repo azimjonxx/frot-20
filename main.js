1d0e240f6e35951c1db1d99bae0413777e18b4db
azimjonxatamov <azimjonxatamov6@gmail.com>
2024-07-09 11:20:03 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,56 @@
+
+
+
+
+let a = ['banan', 'gilos', 'uzum', 'shoptoli']
+console.log(a)
+alert('Boshlanish arryning uzunligi: 4')
+let b = confirm('siznig arrayingizdan malumotlar olib tashamochimiz')
+
+if(b == true){
+
+a.pop()
+console.log(a)
+alert("arraynibg usunligi: 3")
+}
+if(b == false){
+alert("Arrayning uzunligi: 4")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
